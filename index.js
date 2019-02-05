179c7381b113faca82cd1396f07e9fc47260fb6f
Jordan Duran <jordanduranm@gmail.com>
2019-02-05 04:39:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-var recipes = new Object ({})
+var recipes = {};
 
- var obj = { prop: 1 }
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({},obj,{[key]:value})
+ function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
 }
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
 }
-function deleteFromObjectByKey(object, key) {
-  delete object.key;
-  return obj
+
+ function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone;
 }
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
-}
+
+ function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+ }
